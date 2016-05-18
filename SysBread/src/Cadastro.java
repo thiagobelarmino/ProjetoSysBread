52ac1521ac81eebd0f0fc5e67571c928d65c5d3a
Felipe dos Santos Carneiro <felipedossantos896@gmail.com>
2016-05-17 21:25:40 -0300

    Add files via upload
    Sistema de sms

---
+++ SysBread/src/Cadastro.java
@@ -0,0 +1,18 @@
+public class Cadastro {
+	
+	private String nome;
+	private String numero;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+}
